a3c3d5f903aabca64656655dd138ce4443c96788
Mike Nichols <nichols.mike.s@gmail.com>
2017-07-09 16:03:00 -0600

    fix files

--- ios/AWSRNCognitoUserPools/AWSRNCognitoUserPools.h
+++ ios/AWSRNCognitoUserPools/AWSRNCognitoUserPools.h
@@ -1,13 +1,28 @@
 //
 //  AWSRNCognitoUserPools.h
-//  AWSRNCognitoUserPools
+//  mobile
 //
-//  Created by Mike Nichols on 7/5/17.
+//  Created by Mike Nichols on 1/7/17.
 //  Copyright © 2017 Facebook. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef AWSRNCognitoUserPools_h
+#define AWSRNCognitoUserPools_h
 
-@interface AWSRNCognitoUserPools : NSObject
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
+#import <AWSCognitoIdentityProvider/AWSCognitoIdentityProvider.h>
+
+@interface AWSRNCognitoUserPools: RCTEventEmitter <
+RCTBridgeModule,
+AWSCognitoIdentityInteractiveAuthenticationDelegate,
+AWSCognitoIdentityPasswordAuthentication,
+AWSCognitoIdentityMultiFactorAuthentication,
+AWSCognitoIdentityRememberDevice
+>
+
 
 @end
+
+
+#endif /* AWSRNCognitoUserPools_h */
